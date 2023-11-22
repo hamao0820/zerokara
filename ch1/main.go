e29fc2ffbe6bc6d17ddda4ca49062f4003414ead
浜田　俊祐 <hamashun0820@icloud.com>
2023-11-22 14:33:28 +0900

    add: 1.6.1

--- ch1/main.go
+++ ch1/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"math"
 
 	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 )
 
 func main() {
@@ -54,7 +57,7 @@ func main() {
 	// C := mat.NewDense(2, 2, []float64{0, 1, 2, 0})
 	// C.Add(A, B)
 
-	// A := mat.NewDense(1, 3, []float64{1, 2, 3})
+	A := mat.NewDense(1, 3, []float64{1, 2, 3})
 	// B := mat.NewDense(1, 3, []float64{2, 4, 6})
 	// C := mat.NewDense(1, 3, []float64{0, 0, 0})
 	// C.Add(A, B)
@@ -88,82 +91,114 @@ func main() {
 	// B := mat.NewDense(2, 1, []float64{10, 20})
 	// mulElem(A, B) // panic
 
-	X := mat.NewDense(3, 2, []float64{51, 55, 14, 19, 0, 4})
-	matPrint(X)
-	matPrint(X.RowView(0))
-	matPrint(X.ColView(1))
-	fmt.Println(X.At(0, 1))
+	// X := mat.NewDense(3, 2, []float64{51, 55, 14, 19, 0, 4})
+	// matPrint(X)
+	// matPrint(X.RowView(0))
+	// matPrint(X.ColView(1))
+	// fmt.Println(X.At(0, 1))
 
-	r, _ := X.Dims()
-	for i := 0; i < r; i++ {
-		matPrint(X.RowView(i))
-	}
-	matPrint(X)
-	fmt.Println(flatten(X))
+	// r, _ := X.Dims()
+	// for i := 0; i < r; i++ {
+	// 	matPrint(X.RowView(i))
+	// }
+	// matPrint(X)
+	// fmt.Println(flatten(X))
 
-	matPrint(get(flatten(X), mat.NewDense(1, 3, []float64{0, 2, 4})))
+	// matPrint(get(flatten(X), mat.NewDense(1, 3, []float64{0, 2, 4})))
 
-}
+	p := plot.New()
+	p.Title.Text = "Sin"
+	p.X.Label.Text = "X"
+	p.Y.Label.Text = "Y"
+	p.X.Min = 0
+	p.X.Max = 6
+	p.Y.Min = -1
+	p.Y.Max = 1
 
-func matPrint(X mat.Matrix) {
-	fa := mat.Formatted(X, mat.Prefix(""), mat.Squeeze())
-	fmt.Printf("%v\n", fa)
-}
+	p.Add(plotter.NewGrid())
 
-func add(a, b *mat.Dense) *mat.Dense {
-	var c mat.Dense
-	c.Add(a, b)
-	return &c
-}
-
-func sub(a, b *mat.Dense) *mat.Dense {
-	var c mat.Dense
-	c.Sub(a, b)
-	return &c
-}
-
-func mulElem(a, b *mat.Dense) *mat.Dense {
-	var c mat.Dense
-	c.MulElem(a, b)
-	return &c
-}
-
-func divElem(a, b *mat.Dense) *mat.Dense {
-	var c mat.Dense
-	c.DivElem(a, b)
-	return &c
-}
-
-func mul(a, b *mat.Dense) *mat.Dense {
-	var c mat.Dense
-	c.Mul(a, b)
-	return &c
-}
-
-func scale(a *mat.Dense, f float64) *mat.Dense {
-	var c mat.Dense
-	c.Scale(f, a)
-	return &c
-}
-
-func flatten(a *mat.Dense) []float64 {
-	r, c := a.Dims()
-	fa := make([]float64, r*c)
-	for i := 0; i < r; i++ {
-		for j := 0; j < c; j++ {
-			fa[i*c+j] = a.At(i, j)
-		}
-	}
-	return fa
-}
-
-// アクセスしたい複数のindexを配列で受け取る
-func get(arr []float64, idx *mat.Dense) *mat.Dense {
-	b := []float64{}
-	indices := flatten(idx)
-	for _, v := range indices {
-		b = append(b, arr[int(v)])
+	x := []float64{}
+	y := []float64{}
+	for i := 0; i < 6/0.1; i++ {
+		x = append(x, float64(i)*0.1)
+		y = append(y, math.Sin(float64(i)*0.1))
 	}
 
-	return mat.NewDense(1, len(b), b)
+	pts := make(plotter.XYs, len(x))
+	for i := range pts {
+		pts[i].X = x[i]
+		pts[i].Y = y[i]
+	}
+
+	l, err := plotter.NewLine(pts)
+
+	if err != nil {
+		panic(err)
+	}
+	p.Add(l)
+
+	p.Save(vg.Inch*5, vg.Inch*5, "sin.png")
 }
+
+// func matPrint(X mat.Matrix) {
+// 	fa := mat.Formatted(X, mat.Prefix(""), mat.Squeeze())
+// 	fmt.Printf("%v\n", fa)
+// }
+
+// func add(a, b *mat.Dense) *mat.Dense {
+// 	var c mat.Dense
+// 	c.Add(a, b)
+// 	return &c
+// }
+
+// func sub(a, b *mat.Dense) *mat.Dense {
+// 	var c mat.Dense
+// 	c.Sub(a, b)
+// 	return &c
+// }
+
+// func mulElem(a, b *mat.Dense) *mat.Dense {
+// 	var c mat.Dense
+// 	c.MulElem(a, b)
+// 	return &c
+// }
+
+// func divElem(a, b *mat.Dense) *mat.Dense {
+// 	var c mat.Dense
+// 	c.DivElem(a, b)
+// 	return &c
+// }
+
+// func mul(a, b *mat.Dense) *mat.Dense {
+// 	var c mat.Dense
+// 	c.Mul(a, b)
+// 	return &c
+// }
+
+// func scale(a *mat.Dense, f float64) *mat.Dense {
+// 	var c mat.Dense
+// 	c.Scale(f, a)
+// 	return &c
+// }
+
+// func flatten(a *mat.Dense) []float64 {
+// 	r, c := a.Dims()
+// 	fa := make([]float64, r*c)
+// 	for i := 0; i < r; i++ {
+// 		for j := 0; j < c; j++ {
+// 			fa[i*c+j] = a.At(i, j)
+// 		}
+// 	}
+// 	return fa
+// }
+
+// // アクセスしたい複数のindexを配列で受け取る
+// func get(arr []float64, idx *mat.Dense) *mat.Dense {
+// 	b := []float64{}
+// 	indices := flatten(idx)
+// 	for _, v := range indices {
+// 		b = append(b, arr[int(v)])
+// 	}
+
+// 	return mat.NewDense(1, len(b), b)
+// }
