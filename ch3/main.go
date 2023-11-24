3491129d50eb65bab1b65ed5288a2b357699344f
浜田　俊祐 <hamashun0820@icloud.com>
2023-11-24 22:31:44 +0900

    update: 3.2.4 plot sigmoid

--- ch3/main.go
+++ ch3/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"math"
 
-	"github.com/hamao0820/zerokara/util"
 	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 )
 
 func main() {
@@ -12,39 +14,36 @@ func main() {
 	// y := StepFunction(x)
 	// util.MatPrint(y)
 
-	x := mat.NewDense(1, 3, []float64{-1.0, 1.0, 2.0})
-	y := Sigmoid(x)
-	util.MatPrint(y)
+	// x := mat.NewDense(1, 3, []float64{-1.0, 1.0, 2.0})
+	// y := Sigmoid(x)
+	// util.MatPrint(y)
 
-	// p := plot.New()
-	// p.Title.Text = "Step Function"
-	// p.X.Label.Text = "x"
-	// p.Y.Label.Text = "y"
+	p := plot.New()
 
-	// p.X.Min = -6
-	// p.X.Max = 6
-	// p.Y.Min = -0.1
-	// p.Y.Max = 1.1
+	p.X.Min = -6
+	p.X.Max = 6
+	p.Y.Min = -0.1
+	p.Y.Max = 1.1
 
-	// xs := []float64{}
-	// for i := -5.0; i <= 5.0; i += 0.1 {
-	// 	xs = append(xs, i)
-	// }
-	// y := StepFunction(mat.NewDense(1, len(xs), xs))
-	// pts := make(plotter.XYs, len(xs))
-	// for i := range pts {
-	// 	pts[i].X = xs[i]
-	// 	pts[i].Y = y.At(0, i)
-	// }
-	// line, err := plotter.NewLine(pts)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// p.Add(line)
+	xs := []float64{}
+	for i := -5.0; i <= 5.0; i += 0.1 {
+		xs = append(xs, i)
+	}
+	y := Sigmoid(mat.NewDense(1, len(xs), xs))
+	pts := make(plotter.XYs, len(xs))
+	for i := range pts {
+		pts[i].X = xs[i]
+		pts[i].Y = y.At(0, i)
+	}
+	line, err := plotter.NewLine(pts)
+	if err != nil {
+		panic(err)
+	}
+	p.Add(line)
 
-	// if err := p.Save(4*vg.Inch, 4*vg.Inch, "step_function.png"); err != nil {
-	// 	panic(err)
-	// }
+	if err := p.Save(4*vg.Inch, 4*vg.Inch, "sigmoid.png"); err != nil {
+		panic(err)
+	}
 }
 
 func StepFunction(x mat.Matrix) mat.Matrix {
