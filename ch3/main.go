77baa358191bccb71c9c02757d8c4f9fb09891cc
浜田　俊祐 <hamashun0820@icloud.com>
2023-11-24 22:28:08 +0900

    add: 3.2.4

--- ch3/main.go
+++ ch3/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"math"
+
+	"github.com/hamao0820/zerokara/util"
 	"gonum.org/v1/gonum/mat"
-	"gonum.org/v1/plot"
-	"gonum.org/v1/plot/plotter"
-	"gonum.org/v1/plot/vg"
 )
 
 func main() {
@@ -12,35 +12,39 @@ func main() {
 	// y := StepFunction(x)
 	// util.MatPrint(y)
 
-	p := plot.New()
-	p.Title.Text = "Step Function"
-	p.X.Label.Text = "x"
-	p.Y.Label.Text = "y"
+	x := mat.NewDense(1, 3, []float64{-1.0, 1.0, 2.0})
+	y := Sigmoid(x)
+	util.MatPrint(y)
 
-	p.X.Min = -6
-	p.X.Max = 6
-	p.Y.Min = -0.1
-	p.Y.Max = 1.1
+	// p := plot.New()
+	// p.Title.Text = "Step Function"
+	// p.X.Label.Text = "x"
+	// p.Y.Label.Text = "y"
 
-	xs := []float64{}
-	for i := -5.0; i <= 5.0; i += 0.1 {
-		xs = append(xs, i)
-	}
-	y := StepFunction(mat.NewDense(1, len(xs), xs))
-	pts := make(plotter.XYs, len(xs))
-	for i := range pts {
-		pts[i].X = xs[i]
-		pts[i].Y = y.At(0, i)
-	}
-	line, err := plotter.NewLine(pts)
-	if err != nil {
-		panic(err)
-	}
-	p.Add(line)
+	// p.X.Min = -6
+	// p.X.Max = 6
+	// p.Y.Min = -0.1
+	// p.Y.Max = 1.1
 
-	if err := p.Save(4*vg.Inch, 4*vg.Inch, "step_function.png"); err != nil {
-		panic(err)
-	}
+	// xs := []float64{}
+	// for i := -5.0; i <= 5.0; i += 0.1 {
+	// 	xs = append(xs, i)
+	// }
+	// y := StepFunction(mat.NewDense(1, len(xs), xs))
+	// pts := make(plotter.XYs, len(xs))
+	// for i := range pts {
+	// 	pts[i].X = xs[i]
+	// 	pts[i].Y = y.At(0, i)
+	// }
+	// line, err := plotter.NewLine(pts)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// p.Add(line)
+
+	// if err := p.Save(4*vg.Inch, 4*vg.Inch, "step_function.png"); err != nil {
+	// 	panic(err)
+	// }
 }
 
 func StepFunction(x mat.Matrix) mat.Matrix {
@@ -52,5 +56,14 @@ func StepFunction(x mat.Matrix) mat.Matrix {
 	}
 	var y mat.Dense
 	y.Apply(stepFunction, x)
+	return &y
+}
+
+func Sigmoid(x mat.Matrix) mat.Matrix {
+	sigmoid := func(_, _ int, v float64) float64 {
+		return 1 / (1 + math.Exp(-v))
+	}
+	var y mat.Dense
+	y.Apply(sigmoid, x)
 	return &y
 }
