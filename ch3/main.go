821190dbae9d9a0fc9ed5d37b5ce123b4d40738a
浜田　俊祐 <hamashun0820@icloud.com>
2023-11-25 09:24:09 +0900

    add: 3.2.7

--- ch3/main.go
+++ ch3/main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"math"
 
+	"github.com/hamao0820/zerokara/util"
 	"gonum.org/v1/gonum/mat"
-	"gonum.org/v1/plot"
-	"gonum.org/v1/plot/plotter"
-	"gonum.org/v1/plot/vg"
 )
 
 func main() {
@@ -18,43 +16,47 @@ func main() {
 	// y := Sigmoid(x)
 	// util.MatPrint(y)
 
-	p := plot.New()
+	x := mat.NewDense(1, 5, []float64{-1.0, 1.2, 2.3, -3.3, 4.0})
+	y := ReLU(x)
+	util.MatPrint(y)
 
-	p.X.Min = -6
-	p.X.Max = 6
-	p.Y.Min = -0.1
-	p.Y.Max = 1.1
+	// p := plot.New()
 
-	xs := []float64{}
-	for i := -5.0; i <= 5.0; i += 0.1 {
-		xs = append(xs, i)
-	}
-	ySigmoid := StepFunction(mat.NewDense(1, len(xs), xs))
-	yStepFUnction := Sigmoid(mat.NewDense(1, len(xs), xs))
-	ptsSigmoid := make(plotter.XYs, len(xs))
-	ptsStepFunction := make(plotter.XYs, len(xs))
-	for i := range ptsSigmoid {
-		ptsSigmoid[i].X = xs[i]
-		ptsSigmoid[i].Y = ySigmoid.At(0, i)
-		ptsStepFunction[i].X = xs[i]
-		ptsStepFunction[i].Y = yStepFUnction.At(0, i)
-	}
-	sigmoid, err := plotter.NewLine(ptsSigmoid)
-	if err != nil {
-		panic(err)
-	}
-	stepFunction, err := plotter.NewLine(ptsStepFunction)
-	if err != nil {
-		panic(err)
-	}
-	sigmoid.LineStyle.Width = vg.Points(1)
-	sigmoid.LineStyle.Dashes = []vg.Length{vg.Points(2), vg.Points(2)}
-	p.Add(sigmoid)
-	p.Add(stepFunction)
+	// p.X.Min = -6
+	// p.X.Max = 6
+	// p.Y.Min = -0.1
+	// p.Y.Max = 1.1
 
-	if err := p.Save(4*vg.Inch, 4*vg.Inch, "step_function&sigmoid.png"); err != nil {
-		panic(err)
-	}
+	// xs := []float64{}
+	// for i := -5.0; i <= 5.0; i += 0.1 {
+	// 	xs = append(xs, i)
+	// }
+	// ySigmoid := StepFunction(mat.NewDense(1, len(xs), xs))
+	// yStepFUnction := Sigmoid(mat.NewDense(1, len(xs), xs))
+	// ptsSigmoid := make(plotter.XYs, len(xs))
+	// ptsStepFunction := make(plotter.XYs, len(xs))
+	// for i := range ptsSigmoid {
+	// 	ptsSigmoid[i].X = xs[i]
+	// 	ptsSigmoid[i].Y = ySigmoid.At(0, i)
+	// 	ptsStepFunction[i].X = xs[i]
+	// 	ptsStepFunction[i].Y = yStepFUnction.At(0, i)
+	// }
+	// sigmoid, err := plotter.NewLine(ptsSigmoid)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// stepFunction, err := plotter.NewLine(ptsStepFunction)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// sigmoid.LineStyle.Width = vg.Points(1)
+	// sigmoid.LineStyle.Dashes = []vg.Length{vg.Points(2), vg.Points(2)}
+	// p.Add(sigmoid)
+	// p.Add(stepFunction)
+
+	// if err := p.Save(4*vg.Inch, 4*vg.Inch, "step_function&sigmoid.png"); err != nil {
+	// 	panic(err)
+	// }
 }
 
 func StepFunction(x mat.Matrix) mat.Matrix {
@@ -75,5 +77,14 @@ func Sigmoid(x mat.Matrix) mat.Matrix {
 	}
 	var y mat.Dense
 	y.Apply(sigmoid, x)
+	return &y
+}
+
+func ReLU(x mat.Matrix) mat.Matrix {
+	relu := func(_, _ int, v float64) float64 {
+		return math.Max(0, v)
+	}
+	var y mat.Dense
+	y.Apply(relu, x)
 	return &y
 }
