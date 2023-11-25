94a611b0bd4cd33955675842f98659bb7d85e8e6
浜田　俊祐 <hamashun0820@icloud.com>
2023-11-25 09:27:03 +0900

    update: 3.2.7 plot ReLU

--- ch3/main.go
+++ ch3/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"math"
 
-	"github.com/hamao0820/zerokara/util"
 	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 )
 
 func main() {
@@ -16,47 +18,36 @@ func main() {
 	// y := Sigmoid(x)
 	// util.MatPrint(y)
 
-	x := mat.NewDense(1, 5, []float64{-1.0, 1.2, 2.3, -3.3, 4.0})
-	y := ReLU(x)
-	util.MatPrint(y)
+	// x := mat.NewDense(1, 5, []float64{-1.0, 1.2, 2.3, -3.3, 4.0})
+	// y := ReLU(x)
+	// util.MatPrint(y)
 
-	// p := plot.New()
+	p := plot.New()
 
-	// p.X.Min = -6
-	// p.X.Max = 6
-	// p.Y.Min = -0.1
-	// p.Y.Max = 1.1
+	p.X.Min = -6
+	p.X.Max = 6
+	p.Y.Min = -0.1
+	p.Y.Max = 5.5
 
-	// xs := []float64{}
-	// for i := -5.0; i <= 5.0; i += 0.1 {
-	// 	xs = append(xs, i)
-	// }
-	// ySigmoid := StepFunction(mat.NewDense(1, len(xs), xs))
-	// yStepFUnction := Sigmoid(mat.NewDense(1, len(xs), xs))
-	// ptsSigmoid := make(plotter.XYs, len(xs))
-	// ptsStepFunction := make(plotter.XYs, len(xs))
-	// for i := range ptsSigmoid {
-	// 	ptsSigmoid[i].X = xs[i]
-	// 	ptsSigmoid[i].Y = ySigmoid.At(0, i)
-	// 	ptsStepFunction[i].X = xs[i]
-	// 	ptsStepFunction[i].Y = yStepFUnction.At(0, i)
-	// }
-	// sigmoid, err := plotter.NewLine(ptsSigmoid)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// stepFunction, err := plotter.NewLine(ptsStepFunction)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// sigmoid.LineStyle.Width = vg.Points(1)
-	// sigmoid.LineStyle.Dashes = []vg.Length{vg.Points(2), vg.Points(2)}
-	// p.Add(sigmoid)
-	// p.Add(stepFunction)
+	xs := []float64{}
+	for i := -5.0; i <= 5.0; i += 0.1 {
+		xs = append(xs, i)
+	}
+	y := ReLU(mat.NewDense(1, len(xs), xs))
+	pts := make(plotter.XYs, len(xs))
+	for i := range pts {
+		pts[i].X = xs[i]
+		pts[i].Y = y.At(0, i)
+	}
+	l, err := plotter.NewLine(pts)
+	if err != nil {
+		panic(err)
+	}
+	p.Add(l)
 
-	// if err := p.Save(4*vg.Inch, 4*vg.Inch, "step_function&sigmoid.png"); err != nil {
-	// 	panic(err)
-	// }
+	if err := p.Save(4*vg.Inch, 4*vg.Inch, "relu.png"); err != nil {
+		panic(err)
+	}
 }
 
 func StepFunction(x mat.Matrix) mat.Matrix {
