8ea186f3f14994a17d80f8781a1d05e52f1ba383
浜田　俊祐 <hamashun0820@icloud.com>
2023-11-25 13:48:12 +0900

    add: 3.4.3

--- ch3/main.go
+++ ch3/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"math"
 
 	"github.com/hamao0820/zerokara/util"
@@ -66,43 +65,48 @@ func main() {
 	// Y := util.Mul(X, W)
 	// util.MatPrint(Y)
 
+	// X := mat.NewDense(1, 2, []float64{1.0, 0.5})
+	// W1 := mat.NewDense(2, 3, []float64{0.1, 0.3, 0.5, 0.2, 0.4, 0.6})
+	// B1 := mat.NewDense(1, 3, []float64{0.1, 0.2, 0.3})
+
+	// fmt.Println(X.Dims())
+	// fmt.Println(W1.Dims())
+	// fmt.Println(B1.Dims())
+
+	// A1 := util.Add(util.Mul(X, W1), B1)
+	// Z1 := Sigmoid(A1)
+
+	// util.MatPrint(A1)
+	// util.MatPrint(Z1)
+
+	// W2 := mat.NewDense(3, 2, []float64{0.1, 0.4, 0.2, 0.5, 0.3, 0.6})
+	// B2 := mat.NewDense(1, 2, []float64{0.1, 0.2})
+
+	// fmt.Println(Z1.Dims())
+	// fmt.Println(W2.Dims())
+	// fmt.Println(B2.Dims())
+
+	// A2 := util.Add(util.Mul(Z1, W2), B2)
+	// Z2 := Sigmoid(A2)
+
+	// util.MatPrint(A2)
+	// util.MatPrint(Z2)
+
+	// W3 := mat.NewDense(2, 2, []float64{0.1, 0.3, 0.2, 0.4})
+	// B3 := mat.NewDense(1, 2, []float64{0.1, 0.2})
+
+	// fmt.Println(Z2.Dims())
+	// fmt.Println(W3.Dims())
+	// fmt.Println(B3.Dims())
+
+	// A3 := util.Add(util.Mul(Z2, W3), B3)
+	// Y := IdentityFunction(A3)
+
+	// util.MatPrint(Y)
+
+	network := NewNetwork()
 	X := mat.NewDense(1, 2, []float64{1.0, 0.5})
-	W1 := mat.NewDense(2, 3, []float64{0.1, 0.3, 0.5, 0.2, 0.4, 0.6})
-	B1 := mat.NewDense(1, 3, []float64{0.1, 0.2, 0.3})
-
-	fmt.Println(X.Dims())
-	fmt.Println(W1.Dims())
-	fmt.Println(B1.Dims())
-
-	A1 := util.Add(util.Mul(X, W1), B1)
-	Z1 := Sigmoid(A1)
-
-	util.MatPrint(A1)
-	util.MatPrint(Z1)
-
-	W2 := mat.NewDense(3, 2, []float64{0.1, 0.4, 0.2, 0.5, 0.3, 0.6})
-	B2 := mat.NewDense(1, 2, []float64{0.1, 0.2})
-
-	fmt.Println(Z1.Dims())
-	fmt.Println(W2.Dims())
-	fmt.Println(B2.Dims())
-
-	A2 := util.Add(util.Mul(Z1, W2), B2)
-	Z2 := Sigmoid(A2)
-
-	util.MatPrint(A2)
-	util.MatPrint(Z2)
-
-	W3 := mat.NewDense(2, 2, []float64{0.1, 0.3, 0.2, 0.4})
-	B3 := mat.NewDense(1, 2, []float64{0.1, 0.2})
-
-	fmt.Println(Z2.Dims())
-	fmt.Println(W3.Dims())
-	fmt.Println(B3.Dims())
-
-	A3 := util.Add(util.Mul(Z2, W3), B3)
-	Y := IdentityFunction(A3)
-
+	Y := Forward(network, X)
 	util.MatPrint(Y)
 }
 
@@ -138,4 +142,29 @@ func ReLU(x mat.Matrix) mat.Matrix {
 
 func IdentityFunction(x mat.Matrix) mat.Matrix {
 	return x
+}
+
+func NewNetwork() map[string]mat.Matrix {
+	network := map[string]mat.Matrix{}
+	network["W1"] = mat.NewDense(2, 3, []float64{0.1, 0.3, 0.5, 0.2, 0.4, 0.6})
+	network["b1"] = mat.NewDense(1, 3, []float64{0.1, 0.2, 0.3})
+	network["W2"] = mat.NewDense(3, 2, []float64{0.1, 0.4, 0.2, 0.5, 0.3, 0.6})
+	network["b2"] = mat.NewDense(1, 2, []float64{0.1, 0.2})
+	network["W3"] = mat.NewDense(2, 2, []float64{0.1, 0.3, 0.2, 0.4})
+	network["b3"] = mat.NewDense(1, 2, []float64{0.1, 0.2})
+	return network
+}
+
+func Forward(network map[string]mat.Matrix, x mat.Matrix) mat.Matrix {
+	W1, W2, W3 := network["W1"], network["W2"], network["W3"]
+	b1, b2, b3 := network["b1"], network["b2"], network["b3"]
+
+	A1 := util.Add(util.Mul(x, W1), b1)
+	Z1 := Sigmoid(A1)
+	A2 := util.Add(util.Mul(Z1, W2), b2)
+	Z2 := Sigmoid(A2)
+	A3 := util.Add(util.Mul(Z2, W3), b3)
+	Y := IdentityFunction(A3)
+
+	return Y
 }
