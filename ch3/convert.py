38de9b29e737d57458fd0b09dcc4ae2d2e418068
浜田　俊祐 <hamashun0820@icloud.com>
2023-11-27 16:14:38 +0900

    add: npyファイルに変換

--- ch3/convert.py
+++ ch3/convert.py
@@ -2,11 +2,19 @@ import pickle
 import numpy as np
 
 
-def convert():
+def convertPklToNpz():
     with open("data/sample_weight.pkl", "rb") as f:
         data = pickle.load(f)
         np.savez("data/sample_weight", **data)
 
 
+def convertNpzToNpys():
+    with open("data/sample_weight.npz", "rb") as f:
+        data = np.load(f)
+        for key, value in data.items():
+            np.save("data/weights/{}.npy".format(key), value)
+
+
 if __name__ == "__main__":
-    convert()
+    # convertPklToNpz()
+    convertNpzToNpys()
