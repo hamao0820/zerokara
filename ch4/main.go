a5aade43433ef1d85258784c7f3fcd4d83014374
浜田　俊祐 <hamashun0820@icloud.com>
2023-11-28 23:52:20 +0900

    add: 4.2.2

--- ch4/main.go
+++ ch4/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/hamao0820/zerokara/matrix"
 )
@@ -10,14 +11,22 @@ func main() {
 	t := matrix.New1D(0, 0, 1, 0, 0, 0, 0, 0, 0, 0)
 	y := matrix.New1D(0.1, 0.05, 0.6, 0, 0.05, 0.1, 0, 0.1, 0, 0)
 	fmt.Println(SumSquaredError(y, t))
+	fmt.Println(CrossEntropyError(y, t))
 
 	y = matrix.New1D(0.1, 0.05, 0.1, 0, 0.05, 0.1, 0, 0.6, 0, 0)
 	fmt.Println(SumSquaredError(y, t))
-
+	fmt.Println(CrossEntropyError(y, t))
 }
 
 func SumSquaredError(y, t matrix.Matrix) float64 {
 	return 0.5 * y.CopySub(t).CopyApply(func(x float64) float64 {
 		return x * x
 	}).Sum().At(0, 0)
+}
+
+func CrossEntropyError(y, t matrix.Matrix) float64 {
+	delta := 1e-7
+	return -y.CopyApply(func(x float64) float64 {
+		return math.Log(x + delta)
+	}).CopyMul(t).Sum().At(0, 0)
 }
